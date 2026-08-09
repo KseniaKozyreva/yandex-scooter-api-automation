cff29eb0333d458aa839f49d27742428731ea54c
Ksenia Kozyreva <kseniakozyreva5@gmail.com>
2026-08-09 15:17:55 +0300

    Return to Firefox with xvfb virtual display

--- conftest.py
+++ conftest.py
@@ -1,21 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-import os
 
 @pytest.fixture
 def driver():
-    if os.environ.get('GITHUB_ACTIONS') == 'true':
-        options = ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
-        driver = webdriver.Chrome(options=options)
-    else:
-        driver = webdriver.Firefox()
-        driver.maximize_window()
+    # Наш любимый Firefox, который работает без единой ошибки!
+    driver = webdriver.Firefox()
+    driver.maximize_window()
     
     yield driver
+    
     driver.quit()
