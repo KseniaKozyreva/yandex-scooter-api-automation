68ec0f912fa7c1785954184d44f037ce5a946a99
Ksenia Kozyreva <kseniakozyreva5@gmail.com>
2026-08-09 13:20:44 +0300

    Switch to headless Chrome for stable CI run

--- conftest.py
+++ conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import os
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Firefox()
-    driver.maximize_window()
+    if os.environ.get('GITHUB_ACTIONS') == 'true':
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        driver = webdriver.Chrome(options=options)
+    else:
+        driver = webdriver.Firefox()
+        driver.maximize_window()
     
     yield driver
-    
     driver.quit()
